b66c27d7188fa11d4ad2cfb8dfc16492984683bf
Hemant <gouthamchoudary0@gmail.com>
2025-08-22 19:35:42 +0530

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const routes = require('./routes');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(fileUpload());
+
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/healthlock', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.use('/api', routes);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
