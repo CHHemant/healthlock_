e00186c3c1f994a341a6a343e4c6cc6c407efb87
Hemant <gouthamchoudary0@gmail.com>
2025-08-22 19:48:04 +0530

    Create encrypt.js

---
+++ backend/utils/encrypt.js
@@ -0,0 +1,8 @@
+// NOTE: For demo purposes only! Use strong encryption in production.
+exports.encryptFile = (filePath) => {
+  // Implement file encryption here (omitted for brevity)
+  return filePath;
+};
+exports.decryptFile = (filePath) => {
+  return filePath;
+};
